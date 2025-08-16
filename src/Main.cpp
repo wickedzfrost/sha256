ff9672adc15a425a8e117ff8033bfff2ffb7112d
Frost <wickedzfrost@gmail.com>
2025-08-16 12:46:29 +0800

    Update messageSchedule to process more than 512 bits

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,6 +9,42 @@
 
 using Binary = std::vector<std::uint8_t>;
 
+constexpr std::array<std::uint32_t, 8> sqrtHash
+{
+    0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19,
+};
+
+constexpr std::array<std::uint32_t, 64> cbrtHash
+{
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+};
+
+namespace HashState
+{
+    std::uint32_t h0{ sqrtHash[0] };
+    std::uint32_t h1{ sqrtHash[1] };
+    std::uint32_t h2{ sqrtHash[2] };
+    std::uint32_t h3{ sqrtHash[3] };
+    std::uint32_t h4{ sqrtHash[4] };
+    std::uint32_t h5{ sqrtHash[5] };
+    std::uint32_t h6{ sqrtHash[6] };
+    std::uint32_t h7{ sqrtHash[7] };
+}
+
 std::vector<std::uint8_t> toBinary(const std::string& message)
 {
     return std::vector<std::uint8_t>{message.begin(), message.end()};
@@ -114,45 +150,51 @@ std::uint32_t maj(const std::array<std::uint32_t, 3>& bits)
 }
 
 // binary is multiple of 512
-std::array<std::uint32_t, 64> createMessageSchedule(const Binary& binary)
+std::vector<std::array<std::uint32_t, 64>> createMessageSchedule(const Binary& binary)
 {
-    std::array<std::uint32_t, 64> messageBlock{};
+    std::vector<std::array<uint32_t, 64>> result{};
 
+    for (std::size_t chunkStart{0}; chunkStart < binary.size(); chunkStart += 64)
     {
-        std::uint32_t word{};
-        std::size_t counter{ 0 };
-        for (int i{ 0 }; i < 64; ++i)
+        std::array<std::uint32_t, 64> messageBlock{};
         {
-            // Whatever the fuck this is?????
-            word |= binary[i] << (4 - ((i + 1) % 4) % 4) * 8;
-    
-            if ((i + 1) % 4 == 0)
+            std::uint32_t word{};
+            std::size_t counter{ 0 };
+            for (int j{ 0 }; j < 64; ++j)
             {
-                messageBlock[counter] = word;
-                word = uint32_t{};
-                ++counter;
+                // Whatever the fuck this is?????
+                word |= binary[j] << (4 - ((j + 1) % 4) % 4) * 8;
+    
+                if ((j + 1) % 4 == 0)
+                {
+                    messageBlock[counter] = word;
+                    word = uint32_t{};
+                    ++counter;
+                }
             }
         }
+
+        for (int i{ 16 }; i < 64; ++i)
+        {
+            messageBlock[i] = sigmaOne(messageBlock[i - 2]) + messageBlock[i - 7] + sigmaZero(messageBlock[i - 15]) + messageBlock[i - 16];
+        }
+
+        result.push_back(messageBlock);
     }
 
-    for (int i{ 16 }; i < 64; ++i)
-    {
-        messageBlock[i] = sigmaOne(messageBlock[i - 2]) + messageBlock[i - 7] + sigmaZero(messageBlock[i - 15]) + messageBlock[i - 16];
-    }
-
-    return messageBlock;
+    return result;
 }
 
-std::array<std::uint32_t, 8> compressMessage(const std::array<std::uint32_t, 8>& sqrtConstants, const std::array<std::uint32_t, 64>& cbrtConstants, const std::array<std::uint32_t, 64>& messageSchedule)
+void compressMessage(const std::array<std::uint32_t, 64>& cbrtConstants, const std::array<std::uint32_t, 64>& messageSchedule)
 {
-    std::uint32_t a{ sqrtConstants[0] };
-    std::uint32_t b{ sqrtConstants[1] };
-    std::uint32_t c{ sqrtConstants[2] };
-    std::uint32_t d{ sqrtConstants[3] };
-    std::uint32_t e{ sqrtConstants[4] };
-    std::uint32_t f{ sqrtConstants[5] };
-    std::uint32_t g{ sqrtConstants[6] };
-    std::uint32_t h{ sqrtConstants[7] };
+    std::uint32_t a{ HashState::h0 };
+    std::uint32_t b{ HashState::h1 };
+    std::uint32_t c{ HashState::h2 };
+    std::uint32_t d{ HashState::h3 };
+    std::uint32_t e{ HashState::h4 };
+    std::uint32_t f{ HashState::h5 };
+    std::uint32_t g{ HashState::h6 };
+    std::uint32_t h{ HashState::h7 };
 
     for (int i{ 0 }; i < 64; ++i)
     {
@@ -171,16 +213,26 @@ std::array<std::uint32_t, 8> compressMessage(const std::array<std::uint32_t, 8>&
         a = temp1 + temp2;
     }
 
-    a += sqrtConstants[0];
-    b += sqrtConstants[1];
-    c += sqrtConstants[2];
-    d += sqrtConstants[3];
-    e += sqrtConstants[4];
-    f += sqrtConstants[5];
-    g += sqrtConstants[6];
-    h += sqrtConstants[7];
+    HashState::h0 += a;
+    HashState::h1 += b;
+    HashState::h2 += c;
+    HashState::h3 += d;
+    HashState::h4 += e;
+    HashState::h5 += f;
+    HashState::h6 += g;
+    HashState::h7 += h;
+}
 
-    return { a, b, c, d, e, f, g, h };
+std::array<std::uint32_t, 8> compressMessage(const std::vector<std::array<std::uint32_t, 64>>& messageSchedule)
+{
+    using namespace HashState;
+
+    for (const std::array<std::uint32_t, 64>&messageChunk : messageSchedule)
+    {
+        compressMessage(cbrtHash, messageChunk);
+    }
+
+    return { h0, h1, h2, h3, h4, h5, h6, h7 };
 }
 
 int main(int argc, [[maybe_unused]] char* argv[])
@@ -201,33 +253,9 @@ int main(int argc, [[maybe_unused]] char* argv[])
     Binary binary{ toBinary(message) };
     binary = preprocess(binary, messageBitSize);
 
-    std::array<std::uint32_t, 64> messageSchedule{ createMessageSchedule(binary) };
+    std::vector<std::array<std::uint32_t, 64>> messageSchedule{ createMessageSchedule(binary) };
 
-    constexpr std::array<std::uint32_t, 8> sqrtHash
-    {
-        0x6a09e667,
-        0xbb67ae85,
-        0x3c6ef372,
-        0xa54ff53a,
-        0x510e527f,
-        0x9b05688c,
-        0x1f83d9ab,
-        0x5be0cd19,
-    };
-
-    constexpr std::array<std::uint32_t, 64> cbrtHash
-    {
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-    };
-
-    std::array<std::uint32_t, 8> hash{ compressMessage(sqrtHash, cbrtHash, messageSchedule) };
+    std::array<std::uint32_t, 8> hash{ compressMessage(messageSchedule) };
 
     std::cout << "\nSHA-256:\n";
     for (int i{ 0 }; i < 8; ++i)
